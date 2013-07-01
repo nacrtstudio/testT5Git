0a9e0b2f516dea0e8f11af27ff49021a3a4a767b
Njegovan Obradovic <nobradovic83@gmail.com>
2013-07-01 23:31:46 +0200

    test from eclipse

--- src/main/java/com/nacrtstudio/pages/Contact.java
+++ src/main/java/com/nacrtstudio/pages/Contact.java
@@ -1,6 +1,21 @@
 package com.nacrtstudio.pages;
 
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.hibernate.annotations.CommitAfter;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.hibernate.Session;
+
+import com.nacrtstudio.entities.Address;
+
 public class Contact
 {
-
+@Inject
+private Session session;
+@Property
+private Address address;
+@CommitAfter
+public Object onSuccess(){
+	session.persist(address);
+	return Index.class;
+}
 }
--- src/main/java/com/nacrtstudio/pages/Index.java
+++ src/main/java/com/nacrtstudio/pages/Index.java
@@ -1,11 +1,16 @@
 package com.nacrtstudio.pages;
 
 import java.util.Date;
+import java.util.List;
+
 import org.apache.tapestry5.annotations.*;
 import org.apache.tapestry5.ioc.annotations.*;
 import org.apache.tapestry5.corelib.components.*;
 import org.apache.tapestry5.SymbolConstants;
 import org.apache.tapestry5.alerts.AlertManager;
+import org.hibernate.Session;
+
+import com.nacrtstudio.entities.Address;
 
 /**
  * Start page of application nekretnine.
@@ -46,5 +51,13 @@ public class Index
         alertManager.info("Increment (via Ajax) clicked");
 
         return zone;
+    }
+    
+    @Inject
+    private Session session;
+    
+    @SuppressWarnings("unchecked")
+	public List<Address> getAddresses(){
+    	return session.createCriteria(Address.class).list();
     }
 }
